127ab79612908c409039adde588d27283150ef6d
Nina Bobyleva <2847171@mail.ru>
2025-03-18 19:39:43 +0300

    Удалила лишнее, добавила лоудер

--- src/components/DirectoryOrders/DirectoryOrders.tsx
+++ src/components/DirectoryOrders/DirectoryOrders.tsx
@@ -6,6 +6,7 @@ import { Categories } from "../Categories/Categories";
 import { CategoriesType } from "../../types/types";
 
 export const DirectoryOrders = () => {
+  const [isLoad, setIsLoad] = useState(false);
   const [categories, setCategories] = useState<CategoriesType[]>([]);
   const [activeCategory, setActiveCategory] = useState<string | null>(null);
   const [activeMaterial, setActiveMaterial] = useState<string | null>(null);
@@ -16,9 +17,17 @@ export const DirectoryOrders = () => {
   };
 
   useEffect(() => {
-    getAllMaterials().then((data) => {
-      setCategories(data.results);
-    });
+    setIsLoad(true);
+    getAllMaterials()
+      .then((data) => {
+        setCategories(data.results);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setIsLoad(false);
+      });
   }, []);
   return (
     <div>
@@ -27,19 +36,23 @@ export const DirectoryOrders = () => {
         <ButtonGray title="Добавить выбранное" />
         <img className={styles.img} src="./img/basket.svg" alt="" />
       </div>
-      <ul className={styles.list}>
-        {categories.map((el) => (
-          <Categories
-            key={el.id}
-            materials={el.materials}
-            handleCategoryOpen={handleCategoryOpen}
-            isActive={activeCategory === el.name}
-            activeMaterial={activeMaterial}
-            setActiveMaterial={setActiveMaterial}
-            name={el.name}
-          />
-        ))}
-      </ul>
+      {isLoad ? (
+        <p className={styles.loadMassage}>Данные загружаются</p>
+      ) : (
+        <ul className={styles.list}>
+          {categories.map((el) => (
+            <Categories
+              key={el.id}
+              materials={el.materials}
+              handleCategoryOpen={handleCategoryOpen}
+              isActive={activeCategory === el.name}
+              activeMaterial={activeMaterial}
+              setActiveMaterial={setActiveMaterial}
+              name={el.name}
+            />
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
