77807f476e3072d010095236e2e71706793ceec0
Nina Bobyleva <2847171@mail.ru>
2025-03-18 19:26:48 +0300

    Добавила header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,20 @@
+import styles from './header.module.css'
+import { ButtonGray } from "../ButtonGray/ButtonGray";
+
+export const Header = () => {
+  return (
+    <div className={styles.header}>
+      <div className={styles.orderBox}>
+        <p className={styles.orderTitle}>Заказы</p>
+        <div className={styles.btnBox}>
+          <ButtonGray title="Мои заказы" />
+          <ButtonGray title="API документация" />
+        </div>
+      </div>
+      <div className={styles.loginBox}>
+        <p className={styles.login}>234765@mail.ru</p>
+        <ButtonGray title="Выйти" />
+      </div>
+    </div>
+  );
+};
