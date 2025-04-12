05bd684a8a3e2f917b7fb358c348dd7aec12fbb6
Nina Bobyleva <2847171@mail.ru>
2025-04-12 23:35:29 +0300

    cor

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,6 @@ import styles from "./header.module.css";
 import { ButtonGray } from "../ButtonGray/ButtonGray";
 import { retrieveLaunchParams } from "@telegram-apps/bridge";
 // import { init, backButton } from '@telegram-apps/sdk';
-import { on, postEvent } from "@telegram-apps/bridge";
 import { Link, useNavigate } from "react-router-dom";
 import { paths } from "../../paths";
 
@@ -10,21 +9,6 @@ export const Header = () => {
   const navigate = useNavigate();
   const { tgWebAppData } = retrieveLaunchParams();
   console.log("launchParams", tgWebAppData);
-
-  postEvent("web_app_setup_back_button", { is_visible: true });
-
-  const off = on("back_button_pressed", () => {
-    postEvent("web_app_setup_back_button", { is_visible: false });
-    off();
-  });
-
-  // init();
-  // backButton.mount();
-
-  // const off = backButton.onClick(() => {
-  //   off();
-  //   window.history.back();
-  // });
 
   return (
     <div className={styles.header}>
